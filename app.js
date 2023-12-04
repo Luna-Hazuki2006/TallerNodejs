f8f9f7a2e639f41fe7dc570ce6516549f648e967
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-12-04 14:20:09 -0400

    fasdfasdf

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Hola mundo');
+//const express = require('express');
+//const parser = require('body-parser');
+
+import  parser  from 'body-parser'
+import  express  from 'express';
+
+import { router } from './api/router.js'
+
+const app = express();
+
+app.use(parser.json())
+
+const routerExpress = express.Router();
+
+router(routerExpress)
+
+app.use(routerExpress)
+
+const port = 9000;
+
+app.listen(port, () => {
+    console.log(`Aplicación corriendo en: http://127.0.0.1:${port}`)
+})
