3eb744d8eaded4cf127f7e9aa4b31b268da60501
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-12-06 13:58:58 -0400

    con todo creo

--- api/components/Pokemon/PokemonControlador.js
+++ api/components/Pokemon/PokemonControlador.js
@@ -6,56 +6,56 @@ class PokemonControlador {
     async Crear(request, response) {
         // así se obtiene el token
         // request.headers.authorization
-        const servicio = new PokeusaurioServicio()
+        const servicio = new PokemonServicio()
         try {
-            const {idpokemon, idusuario, estado, ubicacion_encuentro, fecha_encuentro, apodo, favorito} = request.body
-            await servicio.Crear(idpokemon, idusuario, estado, ubicacion_encuentro, fecha_encuentro, apodo, favorito)
+            const {nombre, imagen, tipo, altura, peso, url, idubicacion} = request.body
+            await servicio.Crear(nombre, imagen, tipo, altura, peso, url, idubicacion)
             response.status(200).send({mensaje: "Creaste un usario exitosamente"})
 
         } catch (error) {
             console.error(error)
-            response.status(400).send({"error": "Hubo un problema al crear el pokemon del usuario"})
+            response.status(400).send({"error": "Hubo un problema al crear el pokemon"})
         }
     }
 
     async Modificar(request, response) {
-        const servicio = new PokeusaurioServicio()
+        const servicio = new PokemonServicio()
         try {
-            const {idpokemon, idusuario, estado, ubicacion_encuentro, fecha_encuentro, apodo, favorito} = request.body
+            const {nombre, imagen, tipo, altura, peso, url, idubicacion} = request.body
             const id = request.params.id
-            await servicio.Actualizar(id, idpokemon, idusuario, estado, ubicacion_encuentro, fecha_encuentro, apodo, favorito)
+            await servicio.Actualizar(id, nombre, imagen, tipo, altura, peso, url, idubicacion)
             response.status(200).send({mensaje: "Actualizaste al usuario exitósamente"})
         } catch (error) {
             console.error(error);
-            response.status(400).send({"error": "Hubo un problema al crear el pokemon del usuario"})
+            response.status(400).send({"error": "Hubo un problema al crear el pokemon"})
         }
     }
 
     async Obtener(request, response) {
-        const servicio = new PokeusaurioServicio()
+        const servicio = new PokemonServicio()
         try {
             const todos = await servicio.Obtener()
             response.status(200).send({data: todos})
         } catch (error) {
             console.error(error);
-            response.status(400).send({"error": "Hubo un problema al obtener los pokemons de usuario"})
+            response.status(400).send({"error": "Hubo un problema al obtener los pokemons"})
         }
     }
 
     async ObtenerUno(request, response) {
-        const servicio = new PokeusaurioServicio()
+        const servicio = new PokemonServicio()
         try {
             const id = request.params.id
             const usuario = await servicio.ObtenerUno(id)
             response.status(200).send({data: usuario})
         } catch (error) {
             console.error(error);
-            response.status(400).send({"error": "Hubo un problema al obtener el pokemon del usuario"})
+            response.status(400).send({"error": "Hubo un problema al obtener el pokemon"})
         }
     }
 
     async Eliminar(request, response) {
-        const servicio = new PokeusaurioServicio()
+        const servicio = new PokemonServicio()
         try {
             const id = request.params.id
             await servicio.Eliminar(id)
