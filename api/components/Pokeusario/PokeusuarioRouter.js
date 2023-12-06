ea9fb13cf645e421b58150215a11373d3ed0f0b1
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-12-06 13:19:10 -0400

    con el de sesion

--- api/components/Pokeusario/PokeusuarioRouter.js
+++ api/components/Pokeusario/PokeusuarioRouter.js
@@ -5,25 +5,15 @@ const router = (app) => {
 
     const controlador = new PokeusarioControlador()
 
-    app.get('/Pokeusuarios', (request, response)=> {
-        controlador.Obtener(request, response);
-    });
+    app.get('/Pokeusuarios');
     
-    app.get('/Pokeusuarios/:id', (request, response)=> {
-        controlador.ObtenerUno(request, response)
-    })
+    app.get('/Pokeusuarios/:id', validarId)
     
-    app.post('/Pokeusuarios', (request, response) => {
-        controlador.Crear(request, response)        
-    });
+    app.post('/Pokeusuarios', validarCuerpo);
     
-    app.put('/Pokeusuarios/:id', (request, response)=> {
-        controlador.Modificar(request, response)
-    })
+    app.put('/Pokeusuarios/:id', validarId, validarCuerpo)
     
-    app.delete('/Pokeusuarios/:id', (request, response) =>{
-        controlador.Eliminar(request, response);
-    })
+    app.delete('/Pokeusuarios/:id', validarId)
     
 }
 
--- api/components/Sesion/SesionRouter.js
+++ api/components/Sesion/SesionRouter.js
@@ -5,25 +5,15 @@ const router = (app) => {
 
     const controlador = new SesionControlador()
 
-    app.get('/sesiones', (request, response)=> {
-        controlador.Obtener(request, response);
-    });
+    app.get('/sesiones');
     
-    app.get('/sesiones/:id', (request, response)=> {
-        controlador.ObtenerUno(request, response)
-    })
+    app.get('/sesiones/:id', validarId)
     
-    app.post('/sesiones', (request, response) => {
-        controlador.Crear(request, response)        
-    });
+    app.post('/sesiones', validarCuerpo);
     
-    app.put('/sesiones/:id', (request, response)=> {
-        controlador.Modificar(request, response)
-    })
+    app.put('/sesiones/:id', validarId, validarCuerpo)
     
-    app.delete('/sesiones/:id', (request, response) =>{
-        controlador.Eliminar(request, response);
-    })
+    app.delete('/sesiones/:id', validarId)
     
 }
 
